69e2e43a64187d7e7fc9406f131b8399b9c7f472
João Vitor Oliveira Mendes <joao-vitor-o-mendes@hotmail.com>
2023-10-01 19:17:17 -0300

    Organizando o codigo, faltando apenas adicionar os jogos

--- mgwr-content/registro.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById('form')
-
-
-
-form.addEventListener('submit',(evento)=>{
-    evento.preventDefault()
-    console.log(form.nome.value,form.email.value,form.senha.value)
-
-})
