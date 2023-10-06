59f9d643c64962ea620cc0a8e041579142a10d1c
João Vitor Oliveira Mendes <joao-vitor-o-mendes@hotmail.com>
2023-10-05 21:53:34 -0300

    cmt1

--- content/script/login.js
+++ content/script/login.js
@@ -1,6 +1,22 @@
 //obj
 const luffy = document.querySelector('.luffy')
 const logo = document.querySelector('.logo')
+const buttonCadastro = document.getElementById('cadastro')
+const urlCadastro = "../pagina-cadastro/cadastro.html"
+
+//entrar na pagina de cadastro
+
+function entrar(urlCadastro){
+    let win = window.open(urlCadastro, '_blank')
+    win.focus()
+}
+
+
+buttonCadastro.addEventListener('click', function(){
+    entrar(urlCadastro)
+})
+
+
 //var pre declarada
 let rodada = 1
 //animação luffy
